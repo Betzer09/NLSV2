0e3ca596eaf896d16c91421a0265c1741b104a31
Betzer09 <betzer.austin09@gmail.com>
2021-09-28 22:29:30 -0600

    Added tests

---
+++ MatPro/MatProTests/ScoreManager/ScoreManagerLogicTests.swift
@@ -0,0 +1,21 @@
+//
+//  ScoreManagerLogicTests.swift
+//  MatProTests
+//
+//  Created by Austin Betzer on 9/28/21.
+//
+
+import XCTest
+@testable import MatPro
+
+class ScoreManagerLogicTests: XCTestCase {
+
+    override func setUp() {
+        
+    }
+    
+    override func tearDown() {
+        
+    }
+
+}
