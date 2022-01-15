47d63b5b78033e97f86f113f4ce81ad1546d281a
Betzer09 <betzer.austin09@gmail.com>
2022-01-15 14:00:35 -0700

    Working on saving video

--- MatPro/MatPro/ViewControllers/RecordingViewController.swift
+++ MatPro/MatPro/ViewControllers/RecordingViewController.swift
@@ -8,18 +8,19 @@
 import Foundation
 import UIKit
 import AVFoundation
+import Photos
 
 class RecordingViewController: BaseViewController {
     // MARK: - Outlets
     
     // MARK: - Properties
-    private var session: AVCaptureSession?
-    private let output = AVCapturePhotoOutput()
+    private var captureSession: AVCaptureSession?
+    private let videoOutput = AVCaptureMovieFileOutput()
     private let previewLayer = AVCaptureVideoPreviewLayer()
     
     private let shutterButton: UIButton = {
-        let button = UIButton(frame: CGRect(x: 0, y: 0, width: 100, height: 100))
-        button.layer.cornerRadius = 100
+        let button = UIButton(frame: CGRect(x: 0, y: 0, width: 60, height: 60))
+        button.layer.cornerRadius = 30
         button.layer.borderWidth = 10
         button.layer.borderColor = UIColor.white.cgColor
         return button
@@ -32,7 +33,7 @@ class RecordingViewController: BaseViewController {
         super.viewDidLayoutSubviews()
         
         previewLayer.frame = view.bounds
-        shutterButton.center = CGPoint(x: view.frame.size.width/2, y: view.frame.size.height - 200)
+        shutterButton.center = CGPoint(x: view.frame.size.width/2, y: view.frame.size.height - 80)
         
         if let previewLayerConnection = previewLayer.connection,
             previewLayerConnection.isVideoOrientationSupported {
@@ -45,12 +46,8 @@ class RecordingViewController: BaseViewController {
         view.layer.addSublayer(previewLayer)
         view.addSubview(shutterButton)
         UIUtils.lockOrientation(.landscapeRight, andRotateTo: .landscapeRight)
-        shutterButton.addTarget(self, action: #selector(capturePhoto), for: .touchUpInside)
+        shutterButton.addTarget(self, action: #selector(toggleVideoCapture), for: .touchUpInside)
         checkForCameraPermissions()
-    }
-    
-    @objc func capturePhoto() {
-        output.capturePhoto(with: AVCapturePhotoSettings(), delegate: self)
     }
     
     private func updatePreviewLayer(layer: AVCaptureConnection, orientation: AVCaptureVideoOrientation) {
@@ -110,15 +107,15 @@ class RecordingViewController: BaseViewController {
                     session.addInput(input)
                 }
                 
-                if session.canAddOutput(output) {
-                    session.addOutput(output)
+                if session.canAddOutput(videoOutput) {
+                    session.addOutput(videoOutput)
                 }
                 
                 previewLayer.videoGravity = .resizeAspectFill
                 previewLayer.session = session
                 
                 session.startRunning()
-                self.session  = session
+                self.captureSession  = session
             } catch (let error) {
                 print("[RecordingViewController]: Failed to setup camera with error: \(error)")
             }
@@ -127,9 +124,63 @@ class RecordingViewController: BaseViewController {
 
 }
 
-extension RecordingViewController: AVCapturePhotoCaptureDelegate {
-    func photoOutput(_ output: AVCapturePhotoOutput, didFinishProcessingPhoto photo: AVCapturePhoto, error: Error?) {
+extension RecordingViewController: AVCaptureFileOutputRecordingDelegate {
+    func fileOutput(_ output: AVCaptureFileOutput, didFinishRecordingTo outputFileURL: URL, from connections: [AVCaptureConnection], error: Error?) {
         
+        if let error = error {
+            print("[RecordingViewController]: failed to get output URL from av capture output delegate: \(error)")
+            return
+        }
+        
+        PHPhotoLibrary.shared().performChanges({
+            PHAssetChangeRequest.creationRequestForAssetFromVideo(atFileURL: outputFileURL)
+        }) { saved, error in
+            if saved {
+                let alertController = UIAlertController(title: "Your video was successfully saved", message: nil, preferredStyle: .alert)
+                let defaultAction = UIAlertAction(title: "OK", style: .default, handler: nil)
+                alertController.addAction(defaultAction)
+                self.present(alertController, animated: true, completion: nil)
+            }
+        }
+        
+    }
+    
+    @objc func toggleVideoCapture() {
+        guard let captureSession = captureSession else {
+            print("[RecordingViewController]: No capture session was found.")
+            return
+        }
+        
+        if captureSession.isRunning {
+            stopRecording()
+        } else {
+            startVideoCapture()
+        }
+    }
+    
+    func startVideoCapture() {
+        guard let captureSession = captureSession, captureSession.isRunning else {
+            print("[RecordingViewController]: No capture session was found and recording could not be started.")
+            return
+        }
+        let paths = FileManager.default.urls(for: .documentDirectory, in: .userDomainMask)
+        let fileUrl = paths[0].appendingPathComponent("output.mp4")
+        
+        do {
+            try FileManager.default.removeItem(at: fileUrl)
+            videoOutput.startRecording(to: fileUrl, recordingDelegate: self)
+        } catch (let error) {
+            print("[RecordingViewController]: failed to remove item from file manager: \(error)")
+        }
+        
+    }
+    
+    func stopRecording() {
+        guard let captureSession = self.captureSession, captureSession.isRunning else {
+            print("[RecordingViewController]: No capture session was found and recording could not be stopped.")
+            return
+        }
+        videoOutput.stopRecording()
     }
 }
 
@@ -138,6 +189,10 @@ extension RecordingViewController {
     override var prefersStatusBarHidden: Bool {
         return true
     }
+}
+
+enum RecordingViewControllerError: Error {
+    case captureSessionIsMissing
 }
 
 
