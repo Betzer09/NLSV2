a9d3fcfd276ae94bb044aa5d6285b4414a7f8b95
Betzer09 <betzer.austin09@gmail.com>
2022-01-17 11:17:36 -0700

    Added rounded corner to outside border view

--- MatPro/MatPro/App/Recording/MatchViewController.swift
+++ MatPro/MatPro/App/Recording/MatchViewController.swift
@@ -15,7 +15,7 @@ final class MatchViewController: RecordingViewController {
     
     private lazy var scoreView: ScoreView = {
         let view = ScoreView(availableScores: WrestlingScores.folkStyleNeutralScoretypes)
-        view.backgroundColor = .red
+        view.backgroundColor = .white.withAlphaComponent(0.2)
         return view
     }()
     
@@ -26,10 +26,10 @@ final class MatchViewController: RecordingViewController {
         view.addSubview(scoreView)
         scoreView.translatesAutoresizingMaskIntoConstraints = false
         scoreView.widthAnchor.constraint(equalToConstant: 44).isActive = true
-        scoreView.heightAnchor.constraint(equalTo: view.heightAnchor, multiplier: 0.6).isActive = true
+        scoreView.heightAnchor.constraint(equalTo: view.heightAnchor, multiplier: 0.7).isActive = true
         scoreView.leadingAnchor.constraint(equalTo: view.safeAreaLayoutGuide.leadingAnchor, constant: 20).isActive = true
         scoreView.bottomAnchor.constraint(equalTo: view.safeAreaLayoutGuide.bottomAnchor, constant: -20).isActive = true
-        scoreView.bottomAnchor.constraint(equalTo: view.safeAreaLayoutGuide.bottomAnchor, constant: -20).isActive = true
+        scoreView.layer.cornerRadius = 22
     }
     
 }
